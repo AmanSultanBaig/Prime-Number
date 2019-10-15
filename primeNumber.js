5090c4f6aabe5905050b92c4e8bc2beb9ee64873
Aman Sultan Baig <41924807+AmanSultanBaig@users.noreply.github.com>
2019-10-16 00:04:35 +0500

    Create primeNumber.js

---
+++ primeNumber.js
@@ -0,0 +1,8 @@
+var getNumber = prompt('Enter Number (Check Prime Number)');
+
+    let primeNumber = getNumber % 2;
+    if (primeNumber == 0) {
+        alert('not a Prime Number');
+    } else {
+        alert('Prime Number')
+    }
