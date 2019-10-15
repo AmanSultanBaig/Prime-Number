b5eaee721ae95b1b7d6bbe413298f7a5d2c00a55
Aman Sultan Baig <41924807+AmanSultanBaig@users.noreply.github.com>
2019-10-16 00:06:12 +0500

    Update primeNumber.js

--- primeNumber.js
+++ primeNumber.js
@@ -1,6 +1,10 @@
-var getNumber = prompt('Enter Number (Check Prime Number)');
+    // get input from user
+    var getNumber = prompt('Enter Number (Check Prime Number)');
 
+    // try to divide givin number from 2 
     let primeNumber = getNumber % 2;
+    // check if number got divided
+
     if (primeNumber == 0) {
         alert('not a Prime Number');
     } else {
